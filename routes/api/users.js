170fdb0c52d27c5dd5ca504217759ab3ed719ee0
asiaostrowska55 <110023821+asiaostrowska55@users.noreply.github.com>
2024-02-03 17:11:49 +0100

    routes users

---
+++ routes/api/users.js
@@ -0,0 +1,12 @@
+const express = require("express");
+const router = express.Router();
+const controllerUser = require("../../controllers/users");
+const authenticator = require("../../auth/auth");
+
+router.post("/login", controllerUser.logIn);
+
+router.get("/logout", authenticator.auth, controllerUser.logOut);
+
+router.get("/current", authenticator.auth, controllerUser.getCurrent);
+
+module.exports = router;
